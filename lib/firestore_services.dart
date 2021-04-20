e12fba1fd859d8f86d7186f027de0b95486ad9fb
Madhavan S <gitmaddy.16@gmail.com>
2021-04-21 00:15:01 +0530

    Firestore Added

---
+++ lib/firestore_services.dart
@@ -0,0 +1,13 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+// addNewItem() {
+//   FirebaseFirestore.instance.collection('tasks').add({
+//     "title":"Title",
+//     "desc":"Description",
+//     "cluster":"Cluster",
+//     "postedOn": DateTime.now(),
+//     "dueOn":DateTime.now(),
+//   });
+// }
+
+// removeItem(){}
