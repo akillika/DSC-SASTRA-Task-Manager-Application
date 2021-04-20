d901d4035552ca5dbd45a9cb7078193239ab4f6d
Madhavan S <gitmaddy.16@gmail.com>
2021-04-20 22:54:50 +0530

    Registration Working

--- lib/signin.dart
+++ lib/signin.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class SignInPage extends StatefulWidget {
@@ -6,10 +9,48 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  final TextEditingController userName = new TextEditingController();
+  final TextEditingController password = new TextEditingController();
+  FirebaseAuth auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Column(
+        children: [
+          TextField(
+            controller: userName,
+            showCursor: true,
+            decoration: InputDecoration(
+              hintText: 'E-Mail',
+            ),
+          ),
+          TextField(
+            controller: password,
+            showCursor: true,
+            decoration: InputDecoration(
+              hintText: 'Password',
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () async {
+                try {
+                  UserCredential userCredential = await FirebaseAuth.instance
+                      .createUserWithEmailAndPassword(
+                          email: userName.text, password: password.text);
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'weak-password') {
+                    print('The password provided is too weak.');
+                  } else if (e.code == 'email-already-in-use') {
+                    print('The account already exists for that email.');
+                  }
+                } catch (e) {
+                  print(e);
+                }
+              },
+              child: Text('Register'))
+        ],
+      ),
     );
   }
 }
