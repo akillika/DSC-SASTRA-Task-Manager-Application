be8e0bd50239aca766758adafe1448e500adab34
akill <akillika49@gmail.com>
2021-04-21 14:33:32 +0530

    Drawr UI

--- lib/tasks.dart
+++ lib/tasks.dart
@@ -62,6 +62,253 @@ class _TasksState extends State<Tasks> {
         // ),
         Drawer(
           elevation: 10,
+          child: Container(
+            color: Colors.green,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 70),
+                ListTile(
+                  tileColor: widget.cluster == 'DSC Common works'
+                      ? Color(0xffF4B400)
+                      : Colors.transparent,
+                  title: Text(
+                    'DSC Common works',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  onTap: () {
+                    Navigator.of(context).pushAndRemoveUntil(
+                        PageRouteBuilder(
+                            transitionDuration: Duration(seconds: 0),
+                            reverseTransitionDuration: Duration(seconds: 0),
+                            pageBuilder: (context, animation1, animation2) =>
+                                Tasks(
+                                  cluster: "DSC Common works",
+                                )),
+                        (route) => false);
+                  },
+                ),
+                ListTile(
+                  tileColor: widget.cluster == 'Android'
+                      ? Color(0xffF4B400)
+                      : Colors.transparent,
+                  title: Text(
+                    'Android',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  onTap: () {
+                    Navigator.of(context).pushAndRemoveUntil(
+                        PageRouteBuilder(
+                            transitionDuration: Duration(seconds: 0),
+                            reverseTransitionDuration: Duration(seconds: 0),
+                            pageBuilder: (context, animation1, animation2) =>
+                                Tasks(
+                                  cluster: "Android",
+                                )),
+                        (route) => false);
+                  },
+                ),
+                ListTile(
+                  tileColor: widget.cluster == 'AR/VR'
+                      ? Color(0xffF4B400)
+                      : Colors.transparent,
+                  title: Text(
+                    'AR/VR',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  onTap: () {
+                    Navigator.of(context).pushAndRemoveUntil(
+                        PageRouteBuilder(
+                            transitionDuration: Duration(seconds: 0),
+                            reverseTransitionDuration: Duration(seconds: 0),
+                            pageBuilder: (context, animation1, animation2) =>
+                                Tasks(
+                                  cluster: "AR/VR",
+                                )),
+                        (route) => false);
+                  },
+                ),
+                ListTile(
+                  tileColor: widget.cluster == 'Content Writing'
+                      ? Color(0xffF4B400)
+                      : Colors.transparent,
+                  title: Text(
+                    'Content Writing',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  onTap: () {
+                    Navigator.of(context).pushAndRemoveUntil(
+                        PageRouteBuilder(
+                            transitionDuration: Duration(seconds: 0),
+                            reverseTransitionDuration: Duration(seconds: 0),
+                            pageBuilder: (context, animation1, animation2) =>
+                                Tasks(
+                                  cluster: "Content Writing",
+                                )),
+                        (route) => false);
+                  },
+                ),
+                ListTile(
+                  tileColor: widget.cluster == 'Cyber Security'
+                      ? Color(0xffF4B400)
+                      : Colors.transparent,
+                  title: Text(
+                    'Cyber Security',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  onTap: () {
+                    Navigator.of(context).pushAndRemoveUntil(
+                        PageRouteBuilder(
+                            transitionDuration: Duration(seconds: 0),
+                            reverseTransitionDuration: Duration(seconds: 0),
+                            pageBuilder: (context, animation1, animation2) =>
+                                Tasks(
+                                  cluster: "Cyber Security",
+                                )),
+                        (route) => false);
+                  },
+                ),
+                ListTile(
+                  tileColor: widget.cluster == 'Design'
+                      ? Color(0xffF4B400)
+                      : Colors.transparent,
+                  title: Text(
+                    'Design',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  onTap: () {
+                    Navigator.of(context).pushAndRemoveUntil(
+                        PageRouteBuilder(
+                            transitionDuration: Duration(seconds: 0),
+                            reverseTransitionDuration: Duration(seconds: 0),
+                            pageBuilder: (context, animation1, animation2) =>
+                                Tasks(
+                                  cluster: "Design",
+                                )),
+                        (route) => false);
+                  },
+                ),
+                ListTile(
+                  tileColor: widget.cluster == 'Event Coverage'
+                      ? Color(0xffF4B400)
+                      : Colors.transparent,
+                  title: Text(
+                    'Event coverage',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  onTap: () {
+                    Navigator.of(context).pushAndRemoveUntil(
+                        PageRouteBuilder(
+                            transitionDuration: Duration(seconds: 0),
+                            reverseTransitionDuration: Duration(seconds: 0),
+                            pageBuilder: (context, animation1, animation2) =>
+                                Tasks(
+                                  cluster: "Event Coverage",
+                                )),
+                        (route) => false);
+                  },
+                ),
+                ListTile(
+                  tileColor: widget.cluster == 'Flutter'
+                      ? Color(0xffF4B400)
+                      : Colors.transparent,
+                  title: Text(
+                    'Flutter',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  onTap: () {
+                    Navigator.of(context).pushAndRemoveUntil(
+                        PageRouteBuilder(
+                            transitionDuration: Duration(seconds: 0),
+                            reverseTransitionDuration: Duration(seconds: 0),
+                            pageBuilder: (context, animation1, animation2) =>
+                                Tasks(
+                                  cluster: "Flutter",
+                                )),
+                        (route) => false);
+                  },
+                ),
+                ListTile(
+                  tileColor: widget.cluster == 'Google Cloud'
+                      ? Color(0xffF4B400)
+                      : Colors.transparent,
+                  title: Text(
+                    'Google Cloud',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  onTap: () {
+                    Navigator.of(context).pushAndRemoveUntil(
+                        PageRouteBuilder(
+                            transitionDuration: Duration(seconds: 0),
+                            reverseTransitionDuration: Duration(seconds: 0),
+                            pageBuilder: (context, animation1, animation2) =>
+                                Tasks(
+                                  cluster: "Google Cloud",
+                                )),
+                        (route) => false);
+                  },
+                ),
+                ListTile(
+                  tileColor: widget.cluster == 'Machine Learning'
+                      ? Color(0xffF4B400)
+                      : Colors.transparent,
+                  title: Text(
+                    'Machine Learning',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  onTap: () {
+                    Navigator.of(context).pushAndRemoveUntil(
+                        PageRouteBuilder(
+                            transitionDuration: Duration(seconds: 0),
+                            reverseTransitionDuration: Duration(seconds: 0),
+                            pageBuilder: (context, animation1, animation2) =>
+                                Tasks(
+                                  cluster: "Machine Learning",
+                                )),
+                        (route) => false);
+                  },
+                ),
+                ListTile(
+                  tileColor: widget.cluster == 'Web'
+                      ? Color(0xffF4B400)
+                      : Colors.transparent,
+                  title: Text(
+                    'Web',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  onTap: () {
+                    // Navigator.of(context).pushAndRemoveUntil(
+
+                    //     MaterialPageRoute(
+                    //         builder: (context) => Tasks(
+                    //               cluster: "Web",
+                    //             )),
+                    //     (route) => false);
+                    Navigator.of(context).pushAndRemoveUntil(
+                        PageRouteBuilder(
+                            transitionDuration: Duration(seconds: 0),
+                            reverseTransitionDuration: Duration(seconds: 0),
+                            pageBuilder: (context, animation1, animation2) =>
+                                Tasks(
+                                  cluster: "Web",
+                                )),
+                        (route) => false);
+                  },
+                ),
+              ],
+            ),
+          ),
         ),
         Expanded(
             flex: 5,
